c849a95499605656220e82a6a6454fb0f8c0933d
kamal sapkota <kmlspktaa@gmail.com>
2023-09-18 16:12:39 +0100

    first apple data basic operations

--- trading-analytics/read-stock-data.py
+++ trading-analytics/read-stock-data.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.functions import year, month, to_date, concat_ws, dayofmonth
+
 
 class JSONReader:
     def __init__(self, filepath: str):
@@ -18,11 +20,23 @@ class JSONReader:
         df.cache()  # Cache the dataframe
         return df
 
+    @staticmethod
+    def extract_year_month(df: DataFrame) -> DataFrame:
+        """Extract the year and month from the Date column and return the modified DataFrame."""
+        df = df.withColumn("Date", concat_ws("-", year(to_date(df["Date"])), month(to_date(df["Date"])),dayofmonth(to_date(df["Date"]))))
+        return df
+
+
 if __name__ == "__main__":
     json_path = "./apple_stock_data.json"
     reader = JSONReader(json_path)
     df = reader.get_dataframe()
+
+    # Extract year and month from the Date column
+    df = JSONReader.extract_year_month(df)
+
     df.show()
+
     if "_corrupt_record" in df.columns:
         print("Corrupt records found!")
         df.select("_corrupt_record").show()
